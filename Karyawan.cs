f1b018db7f5f1cea6e8cda83c3f8026dee024791
Awaluda <iqbalawaluda@students.amikom.ac.id>
2021-04-13 15:34:41 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3418
+{
+    class Karyawan
+    {
+        public int nim     { get; set; }
+        public string nama { get; set; }
+        public int gaji    { get; set; }
+        public int naikgaji{ get; set; }
+
+        public Karyawan(int nim,string nama,int gaji)
+        {
+            this.nim = nim;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gaji = 0;
+            }
+            else
+            {
+                this.gaji = 0;
+            }
+
+            double tmp = 0;
+            tmp = 0.1 * gaji;
+            naikgaji = Convert.ToInt32(tmp) + gaji;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3418
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public float GajiBulanan { get; set; }
+
+        public void gajiawal()
+        {
+
+            Console.WriteLine("{0} {1}      {2}", Nik, Nama, GajiBulanan);
+
+        }
+
+        public void gajiakhir()
+        {
+            Console.WriteLine("{0} {1}      {2}", Nik, Nama, GajiBulanan + GajiBulanan * 10 / 100);
+        }
+        
+    }
+
+
+}
+    
